6319c1cb7dcf8db4623e9ed519a9ea88c82a3ce6
djohnsoftware <djohnsoftware@gmail.com>
2022-05-06 09:48:27 +0530

    Update
    06-05-2022
    
    By Arjan

--- app/src/main/java/com/djohnsoftware/adsmanager/MainActivity.java
+++ app/src/main/java/com/djohnsoftware/adsmanager/MainActivity.java
@@ -3,10 +3,6 @@ package com.djohnsoftware.adsmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-
-import com.djohnsoftware.adsmanager.ads.CallQureka;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,18 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-            @Override
-            public void run() {
-
-                new CallQureka(MainActivity.this, new CallQureka.QurekaListener() {
-                    @Override
-                    public void next() {
-
-                    }
-                });
-            }
-        },3000);
     }
 }
